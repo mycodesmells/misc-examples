2aefb64c99bc291d3912ec79fa0af9c16a94869f
Paweł Słomka <pslomka@pslomka.com>
2017-07-31 21:40:35 +0200

    Multiple domains with nginx

---
+++ nginx/4-multiple-domains/page2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(rw, "Hello from page2.com!")
+	})
+	http.ListenAndServe(":3000", nil)
+}
